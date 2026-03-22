42d0027986a7ca90cf8f5ac6e4910438ba0bf360
sriramswe <sris70440@email.com>
2026-03-22 20:28:14 +0530

    Auto commit 2026-03-22_20:28:14

--- middleware.js
+++ middleware.js
@@ -2,14 +2,14 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)"
+  "/dashboard(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
+  const { userId, redirectToSignIn } = auth();
 
   if (!userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn();
+    return redirectToSignIn();
   }
 
   return NextResponse.next();
@@ -17,9 +17,7 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)"
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
   ],
 };
