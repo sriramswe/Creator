5a96b1933560586eb69a88d016829645c5763817
sriramswe <sris70440@email.com>
2026-03-22 19:37:27 +0530

    Auto commit 2026-03-22_19:37:27

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)"
+]);
 
 export default clerkMiddleware((auth, req) => {
-  const { userId, redirectToSignIn } = auth();
+  const { userId } = auth();
 
   if (!userId && isProtectedRoute(req)) {
-    return redirectToSignIn();
+    return auth().redirectToSignIn();
   }
 
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)"
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"]
 };
