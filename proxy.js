0bbacb548721cfc25b9ee04c3734291e68776441
sriramswe <sris70440@email.com>
2026-03-23 23:12:27 +0530

    Auto commit 2026-03-23_23:12:27

--- proxy.js
+++ proxy.js
@@ -1,12 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
 export default clerkMiddleware((auth, req) => {
-  const { userId, redirectToSignIn } = auth();
+  const { userId } = auth();
 
   if (!userId) {
-    return redirectToSignIn();
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
   }
 
   return NextResponse.next();
